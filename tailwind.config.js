32476b9f3f1cd4b732e65405e0dfb8b701e8ba28
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-06-25 16:50:43 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  presets: ["./node_modules/tailwindcss-kit/Componentes/lib/mypreset.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
