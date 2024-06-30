bab97e4c2b26941e34c9c19e5e1272867b2ba14d
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-06-30 17:05:02 -0400

    Agregado apartado con sofkilss, scrollreveal a los softskills, apartado de tecnologias, proyectos y contacto

--- src/js/app.js
+++ src/js/app.js
@@ -8,4 +8,15 @@ function startApp()
 {
     // Código para iniciar la aplicación
     console.log('Aplicación iniciada');
+    // scrollReveal();
+}
+
+function scrollReveal()
+{
+    var sr = ScrollReveal();
+
+    // Reveal de los soft skills
+    sr.reveal('.skill-primary-reveal', { delay: 1400, origin: 'left' });
+    sr.reveal('.skill-secondary-reveal', { delay: 1400, origin: 'right' });
+    sr.reveal('.skill-third-reveal', { delay: 1400, origin: 'bottom' });
 }
